eb785109ae9e50f72c5be7c444784670f5d73c5f
sundar316 <16sundar2003@gmail.com>
2021-09-24 10:24:48 +0530

    FF

--- JAVA/Examly/FilmFestival.java
+++ JAVA/Examly/FilmFestival.java
@@ -1,45 +1,38 @@
 package Examly;
 
-import java.util.Scanner;
-
+import java.util.*;
 public class FilmFestival {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
-        int[] L=new int[n];
-        int[] R=new int[n];
+        int[] movie=new int[n];
+        int[] rating=new int[n];
+        int rate=0;
+        int max=0,index=0;
         for(int i=0;i<n;i++)
-            L[i]=sc.nextInt();
+            movie[i]=sc.nextInt();
         for(int i=0;i<n;i++)
-            R[i]=sc.nextInt();
-        int max=0;
-        boolean flag=false;
-        int RI=0;
+            rating[i]=sc.nextInt();
         for(int i=0;i<n;i++)
         {
-            int O=L[i]*R[i];
-            if(O>=max)
+            if(rating[i]*movie[i]>max)
             {
-                int RMax=0;
-                if(O==max && R[i]>=RMax)
+                rate=rating[i];
+                max=rating[i]*movie[i];
+            }
+            else if(rating[i]*movie[i]==max)
+            {
+                if(rating[i]>rate)
                 {
-                    if(R[i]==RMax)
-                    {
-                        System.out.print(RI);
-                        break;
-                    }
-                    RMax=R[i];
-                    RI=i;
-                    flag=true;
+                    index=i;
                 }
-                max=O;
+                if(rating[i]==rate)
+                {
+                    continue;
+                }
             }
         }
-        if(flag)
-        {
-            System.out.print(RI);
-        }
-
+        System.out.print(index+1);
         sc.close();
     }
 }
