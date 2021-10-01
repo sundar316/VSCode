4ae118e71d73893037b8f356bccee832c606b003
sundar316 <16sundar2003@gmail.com>
2021-10-01 18:19:05 +0530

    pyramid

--- JAVA/Samples/Patterns/Pyramid.java
+++ JAVA/Samples/Patterns/Pyramid.java
@@ -6,10 +6,12 @@ public class Pyramid{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        //for downward pyramid
         for(int i=0;i<n;i++)
         {
             if(n%2!=0)
             {
+                //for left side pyramid
                 for(int j=0;j<n;j++)
                 {
                     if(((i+j)%2==0 && i<=j))
@@ -17,6 +19,7 @@ public class Pyramid{
                     else
                         System.out.print("  ");
                 }
+                //for right side pyramid
                 for(int j=1;j<n;j++)
                 {
                     if((i+j)%2==0 && i+j<=n-1)
@@ -27,6 +30,7 @@ public class Pyramid{
             }
             else
             {
+                //for left side pyramid
                 for(int j=1;j<n;j++)
                 {
                     if(((i+j)%2!=0 && i<=j))
@@ -34,6 +38,7 @@ public class Pyramid{
                     else
                         System.out.print("  ");
                 }
+                //for right side pyramid
                 for(int j=0;j<n;j++)
                     {
                         if((i+j)%2!=0 && i+j<=n-1)
@@ -41,6 +46,52 @@ public class Pyramid{
                         else
                             System.out.print("  ");
                     }
+            }
+            System.out.println();
+        }
+
+/*****************************************************************************/
+
+        //for upward pyramid
+        for(int i=1;i<n;i++)
+        {
+            if(n%2==0)
+            {
+                //for left side pyramid
+                for(int j=0;j<n;j++)
+                {
+                    if(((i+j)%2!=0 && i+j>=n-1))
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+                //for right side pyramid
+                for(int j=1;j<n;j++)
+                {
+                    if((i+j)%2==0 && i>=j)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+            }
+            else
+            {
+                //for left side pyramid
+                for(int j=0;j<n;j++)
+                {
+                    if(((i+j)%2==0 && i+j>=n-1))
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+                //for right side pyramid
+                for(int j=1;j<n;j++)
+                {
+                    if((i+j)%2==0 && i>=j)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
             }
             System.out.println();
         }
