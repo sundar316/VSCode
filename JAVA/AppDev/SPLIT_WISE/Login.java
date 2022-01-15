f4920955ef1f402a20c445399101be49ccb638cd
sundar316 <16sundar2003@gmail.com>
2022-01-16 00:33:17 +0530

    SplitWise

---
+++ JAVA/AppDev/SPLIT_WISE/Login.java
@@ -0,0 +1,10 @@
+package AppDev.SPLIT_WISE;
+
+import java.util.Scanner;
+
+public class Login {
+    private Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        
+    }
+}
